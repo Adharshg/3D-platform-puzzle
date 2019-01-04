91309ede31f605a85a72213ce0539735b26a0a2f
AdharshG <34604785+Adharshg@users.noreply.github.com>
2019-01-04 13:28:34 +0530

    minor change commit
    Changed the basic prototype a little bit.
    
    Ready to start level design.
    
    Need to make need game objects.

---
+++ PLATFORM PUZZLE/Assets/Scripts/Attachplatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attachplatform : MonoBehaviour {
+
+	public GameObject Player;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Player.transform.parent = transform;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Player.transform.parent = null;
+		}
+	}
+}
