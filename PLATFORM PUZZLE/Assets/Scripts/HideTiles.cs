fbe8382880f9374763f773f55b458663ea882421
AdharshG <34604785+Adharshg@users.noreply.github.com>
2019-01-19 18:54:12 +0530

    commit 19.1.19
    Imported initial model of the world (that I made myself), and a few from the asset store.
    
    Did a few more things with the project.
    Introduced maze level. Work going on with that.
    
    Should work on changing terrain and map and a few more things.

---
+++ PLATFORM PUZZLE/Assets/Scripts/HideTiles.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTiles : MonoBehaviour {
+ 
+	[SerializeField]
+	private string tileTag;
+ 
+	[SerializeField]
+	private Vector3 tileSize;
+ 
+	[SerializeField]
+	private int maxDistance;
+ 
+	private GameObject[] tiles;
+ 
+	// Use this for initialization
+	void Start () {
+		this.tiles = GameObject.FindGameObjectsWithTag (tileTag);
+		DeactivateDistantTiles ();
+	}
+ 
+	void DeactivateDistantTiles() {
+		Vector3 playerPosition = this.gameObject.transform.position;
+ 
+		foreach (GameObject tile in tiles) {
+			Vector3 tilePosition = tile.gameObject.transform.position + (tileSize / 2f);
+ 
+			float xDistance = Mathf.Abs(tilePosition.x - playerPosition.x);
+			float zDistance = Mathf.Abs(tilePosition.z - playerPosition.z);
+ 
+			if (xDistance + zDistance > maxDistance) {
+				tile.SetActive (false);
+			} else {
+				tile.SetActive (true);
+			}
+		}
+	}
+ 
+	void Update () {
+		DeactivateDistantTiles ();
+	}
+ 
+}
